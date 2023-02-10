5168ec85553205544df10a39c5786313391d4dfd
Jeausthin <Jeausthin@DESKTOP-LO9OVEK>
2023-02-09 22:39:21 -0600

    EA-08-Practicas-2

---
+++ src/main/java/com/DeporteTotal/DeporteTotal/DeporteTotalApplication.java
@@ -0,0 +1,13 @@
+package com.DeporteTotal.DeporteTotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeporteTotalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeporteTotalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DeporteTotal/DeporteTotal/DeporteTotalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DeporteTotal.DeporteTotal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeporteTotalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
